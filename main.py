2f657f48f007e5f1f670a5d14378fca16a6f3852
Vladimir Masarik <masarik.vladimir7@gmail.com>
2020-05-08 17:07:30 +0200

    s

--- main.py
+++ main.py
@@ -226,8 +226,8 @@ projects = [
 
 for p in projects:
 
-    # print("Cloning project", p.name)
-    # repository = cloneProject(p)
+    print("Cloning project", p.name)
+    repository = cloneProject(p)
 
     
     print("Switching revision on project", p.name)
